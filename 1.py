655fdf8426117edddaac5d5c226843e2f5c088a7
AniAsya <guinea-pig@bk.ru>
2021-10-23 22:04:48 +0300

    добавлены пробелы

--- 1.py
+++ 1.py
@@ -1,10 +1,10 @@
-name=input("Введите имя")
+name = input("Введите имя")
 print(name)
-age=int(input("Введите полное количество лет"))
+age = int(input("Введите полное количество лет"))
 print(age)
-height=int(input("Введите Ваш рост"))
+height = int(input("Введите Ваш рост"))
 print(height)
-weight=int(input("Введите Ваш вес"))
+weight = int(input("Введите Ваш вес"))
 print(weight)
 
 
