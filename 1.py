1cbbc727c46456a8ef708cd8e6d3cf969345fcf3
AniAsya <guinea-pig@bk.ru>
2021-10-26 07:06:52 +0300

    done

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
-name = input("Введите имя")
+name = input("Enter your name: ")
 print(name)
-age = int(input("Введите полное количество лет"))
+age = int(input("Enter your age: "))
 print(age)
-height = int(input("Введите Ваш рост"))
+height = int(input("Enter your height: "))
 print(height)
-weight = int(input("Введите Ваш вес"))
+weight = int(input("Enter your weight: "))
 print(weight)
+
 
 
