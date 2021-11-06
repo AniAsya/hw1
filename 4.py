5e96309ab659020e39aa772c7a843bd0b79ae595
AniAsya <guinea-pig@bk.ru>
2021-11-06 22:01:20 +0300

    ok

---
+++ 4.py
@@ -0,0 +1,10 @@
+def my_func(x, y):
+    return x ** y
+    '''
+    Get x raised to the power of y
+    :param x: int > 0
+    :param y: int < 0
+    :return: my_func: float
+    '''
+
+print(my_func(9, -2))
