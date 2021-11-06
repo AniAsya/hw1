4d96b590db543e744e4f514ac309e4b907354aad
AniAsya <guinea-pig@bk.ru>
2021-11-06 18:58:24 +0300

    ok

---
+++ 1.py
@@ -0,0 +1,18 @@
+def division(num_1, num_2):
+    return num_1 / num_2
+    '''
+    Get quotient of numbers
+    :param num_1: float
+    :param num_2: float
+    :return division: float
+    '''
+
+num_1 = float(input("Введите делимое: "))
+num_2 = float(input("Введите делитель: "))
+if num_2 != 0:
+    print(division(num_1, num_2))
+else:
+    print("Делитель не может быть равен 0")
+
+
+
