7893d4144391141e78e96f7d693c5dcea8de6b08
AniAsya <guinea-pig@bk.ru>
2021-12-15 23:04:37 +0300

    ok

---
+++ 1.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+class Trafficlight:
+    colours = ("red", "yellow", "green")
+    delay = (7, 2, 10)
+
+    def __init__(self):
+        self.__colour = "green"
+
+    def running(self):
+        while True:
+            for i in self.colours:
+                self.__colour = i
+                print(self.__colour)
+                sleep(self.delay[self.colours.index(self.__colour)])
+
+traffic_light = Trafficlight()
+traffic_light.running()
